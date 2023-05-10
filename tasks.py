5ce94de33a334d64342faa4a787750f89a7450d4
emilgadjzhiev <108130174+emilgadjzhiev@users.noreply.github.com>
2023-05-10 16:27:35 +0300

    test

--- tasks.py
+++ tasks.py
@@ -1,10 +1,12 @@
-import sys
-import logging
+from RPA.Robocorp.WorkItems import WorkItems
 
 
 def main():
+    library = WorkItems()
+    library.get_input_work_item()
+    variables = library.get_work_item_variables()
     print('UNIQUE IDENTIFIER#1425')
-    print(sys.argv)
+    print(variables)
 
 
 if __name__ == "__main__":
