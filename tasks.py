34969245dc05d84c0faf74e3d43a2373fa922038
emilgadjzhiev <108130174+emilgadjzhiev@users.noreply.github.com>
2023-05-10 16:33:36 +0300

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -5,8 +5,7 @@ def main():
     library = WorkItems()
     library.get_input_work_item()
     variables = library.get_work_item_variables()
-    print('UNIQUE IDENTIFIER#1425')
-    print(variables)
+    print(f'Robot id: {variables["robot_id"]}')
 
 
 if __name__ == "__main__":
