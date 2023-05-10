84a0ab97edff591dbf68f18e814fd1194d7d11ec
emilgadjzhiev <108130174+emilgadjzhiev@users.noreply.github.com>
2023-05-10 15:19:59 +0300

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -1,9 +1,14 @@
 import sys
+import logging
 
 
 def main():
-    print('UNIQUE IDENTIFIER#1425')
-    print(sys.argv)
+    message = 'UNIQUE IDENTIFIER#1425'
+    logging.debug(f'Debug: {message}')
+    logging.info(f'Info: {message}')
+    logging.warning(f'Warning: {message}')
+    logging.error(f'Error: {message}')
+    logging.critical(f'Critical: {message}')
 
 
 if __name__ == "__main__":
